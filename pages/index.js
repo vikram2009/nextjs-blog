a2f8cf77939303ad9aa13c5a6ceaf90e92511f18
vikram2009 <vikramksm@hotmail.com>
2022-06-26 12:55:59 +0530

    v7

--- pages/index.js
+++ pages/index.js
@@ -26,13 +26,13 @@ export default function Home() {
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+          href="https://vikramsrinivas.me"
           target="_blank"
           rel="noopener noreferrer"
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <img src="https://logo-phi.vercel.app/favicon.ico" alt="VS" width={75} height={75} />
           </span>
         </a>
       </footer>
