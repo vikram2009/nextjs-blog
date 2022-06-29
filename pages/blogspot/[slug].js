f947e2e6ec8fea3f9ac5475208eb7a87294bc19c
vikram2009 <vikramksm@hotmail.com>
2022-06-29 16:29:53 +0530

    Update [slug].js

--- pages/blogspot/[slug].js
+++ pages/blogspot/[slug].js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router"
 import styles from '../../styles/Home.module.css'
 
-export default function slug() {
+export default function Slug() {
   const router = useRouter()
   const {slug }= router.query;
   return ( 
