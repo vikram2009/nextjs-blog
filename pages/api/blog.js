6167ef0bf4a804d7c2c9ce1af647de359f6b11dd
vikram2009 <vikramksm@hotmail.com>
2022-07-03 08:14:43 +0530

    v7"

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -11,6 +11,7 @@ for (let index = 0 ; index < data.length ; index++) {
     const item = data[index];
 myfile  = await fs.promises.readFile('blogdata/' + item , 'utf-8' )  
 allblogs.push(JSON.parse(myfile))
+
 }
 res.status(200).json(allblogs);
 
