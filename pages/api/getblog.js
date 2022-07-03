ea5b337942c77f19176e8f37c17a2dab937026e1
vikram2009 <vikramksm@hotmail.com>
2022-07-03 08:36:37 +0530

    v7

--- pages/api/getblog.js
+++ pages/api/getblog.js
@@ -4,15 +4,12 @@ export default function handler(req, res) {
 
   fs.readFile(`blogdata/${req.query.slug}.json` , 'utf-8',  (err , data) => {
    
-  if(err){
-    
-    res.status(200).json({error : "error"}) 
-  }
+  
   
   res.status(200).json(JSON.parse(data))   
 
   })
-  res.status(200).json({name : "Vikram Srinivas"}) 
+  
   
   }
   
