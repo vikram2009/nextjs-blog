df2be4b6f03851a63860a917ffd7877a064d0efa
vikram2009 <vikramksm@hotmail.com>
2022-07-03 08:51:52 +0530

    v7

--- pages/api/getblog.js
+++ pages/api/getblog.js
@@ -1,12 +1,21 @@
-import * as fs from 'fs'
+
 
 export default function handler(req, res) {
+ 
+  const fs = require('fs');
 
   fs.readFile(`blogdata/${req.query.slug}.json` , 'utf-8',  (err , data) => {
    
+    if(err){
+
+      res.status(500).json({error : " error "})
+
+    }else {
+
+      res.status(200).json(JSON.parse(data))   
+    }
   
   
-  res.status(200).json(JSON.parse(data))   
 
   })
   
