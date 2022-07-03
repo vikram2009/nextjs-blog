ea5d452f5a8dbbbfcf12ac509c49837f5570a872
vikram2009 <vikramksm@hotmail.com>
2022-07-03 08:56:50 +0530

    v7

--- pages/api/getblog.js
+++ pages/api/getblog.js
@@ -5,15 +5,9 @@ export default function handler(req, res) {
   const fs = require('fs');
 
   fs.readFile(`blogdata/${req.query.slug}.json` , 'utf-8',  (err , data) => {
-   
-    if(err){
-
-      res.status(500).json({error : " error "})
-
-    }else {
-
+  
       res.status(200).json(JSON.parse(data))   
-    }
+    
   
   
 
