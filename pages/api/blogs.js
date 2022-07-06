e952111348ac10529ecadac57f7d0ba130f86dbb
vikram2009 <vikramksm@hotmail.com>
2022-07-06 17:58:47 +0530

    v7

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -1,23 +1,20 @@
 
+export default function handler(req, res) {
+  
+const data = [
+    {
+    "title": "Best JavaScript Frameworks",
+    "content": "Frontend :- AngularJS & ReactJS  ,  Backend :- NodeJS ,   Frontend && Backend :- NextJS",
+    "author": "Vikram Srinivas",
+    "slug": "bestjsframeworks"
+    },
+    {
+    "title": "What Is JavaScript",
+    "content": "JavaScript Is a High Level Programming Langauge  . JavaScript Is called as the King of Frontend . JavaScript is the language of the web, it is used to make the web look alive by adding motion to it. To be more precise, it’s a programming language that let’s you implement complex and beautiful things/design on web pages. When you notice a web page doing more than just sit there and gawk at you, you can bet that the web page is using JavaScript. ",
+    "author": "Vikram Srinivas",
+    "slug": "whatisjs"
+    }
+    ]
 
-
-
-
-export default async function handler(req, res) {
-const fs = require('fs');
-let data = await fs.promises.readdir('blogdata');
-let myfile;
-let allblogs = []
-
-
-for (let index = 0; index < data.length; index++) {
-    const items = data[index];
-myfile = await fs.promises.readFile('blogdata/' + items , 'utf-8')
-allblogs.push(JSON.parse(myfile))
-
-
-    
-}
-res.status(200).json(allblogs)
-}
-
+res.status(200).json(data);
+  }
--- pages/api/getblog.js
+++ pages/api/getblog.js
@@ -1,14 +1,12 @@
-
-
 export default function handler(req, res) {
  
-  const fs = require('fs');
-
- fs.readFile(`blogdata/${req.query.slug}.json` , 'utf-8' , (err , data) => {
-res.status(200).json(JSON.parse(data))
-
- })
+    const fs = require('fs');
   
+   fs.readFile(`blogdata/${req.query.slug}.json` , 'utf-8' , (err , data) => {
+  res.status(200).json(JSON.parse(data))
   
-  }
-  
+   })
+    
+    
+    }
+    
