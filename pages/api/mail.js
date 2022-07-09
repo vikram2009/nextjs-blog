428bd58e5674f77a883eaf28f474d2e71154d78d
vikram2009 <vikramksm@hotmail.com>
2022-07-09 22:24:58 +0530

    v7

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -1,7 +1,22 @@
+const mail = require('@sendgrid/mail');
+
+mail.setApiKey(process.env.SENDGRID_API_KEY)
 
 export default function handler(req, res) {
   
     const body = JSON.parse(req.body);
-    console.log(body)
+
+    const msg = `Message From Blog :-\r\n Name: ${body.fullname}\r\n Email: ${body.email}\r\n Message: ${body.desc}`;
+  
+    const data = {
+      to: 'vikramksm2009@gmail.com',
+      from: 'vikramksm2009@gmail.com',
+      subject: 'Message From Blog:-',
+      text: msg,
+      html: msg.replace(/\r\n/g, '<br>') 
+    }
+
+  mail.send(data);
+
   
   }
